8caa8e268c9b6e5a9841ef5c839312fda3e032e8
PradipDhanani <pradip.dhanani@gmail.com>
2020-02-08 16:22:14 +0000

    test

---
+++ src/BioData.java
@@ -0,0 +1,21 @@
+public class BioData {
+    public static void main(String[] args) {
+
+
+        String MyName = "Pradip";
+        String SurName = "Dhanani";
+        int Age = 30;
+        String Hobbies = "Playing Cricket";
+
+        System.out.println(MyName);
+        System.out.println("Surname");
+        System.out.println("Age");
+        System.out.println("Hobbies");
+
+
+
+
+
+
+    }
+}
---
+++ src/printpractice/HelloWorld.java
@@ -0,0 +1,26 @@
+package printpractice;
+
+
+public class HelloWorld {
+
+    public static void main(String [] args){
+       // System.out.println("Hello");
+       // System.out.println("World");
+
+       // System.out.print("Hello ");
+        //System.out.print("World");
+
+        System.out.println("My name is = Pradip" );
+        System.out.println("My Surname is = Dhanani");
+        System.out.println("My Age is = 30");
+        System.out.println("My Hobbies is Playing = Cricket");
+
+    }
+
+
+
+
+
+
+
+}
